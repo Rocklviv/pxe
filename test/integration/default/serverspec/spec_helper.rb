afc95bca1c8e11cc9f126e41f5b4d103a9d5b9fd
SiruS <shkurko.alex@gmail.com>
2014-10-18 22:04:49 +0300

    fixed tests to run against serverspec v2

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,9 +1,3 @@
-require "serverspec"
-require "pathname"
+require 'serverspec'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-	c.os = backend.check_os
-end
+set :backend, :exec
